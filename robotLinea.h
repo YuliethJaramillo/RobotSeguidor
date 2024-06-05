72d93e292685a41bb1cfa56cea3f566dd4338897
YuliethJaramillo <yuliethjaramillo@unicauca.edu.co>
2024-06-04 19:58:39 -0500

    Update robotLinea.h

--- robotLinea.h
+++ robotLinea.h
@@ -20,7 +20,7 @@ class robotLinea {
     int INI5;
     
   public:
-    robotLinea(uint8_t velocidadMotorIZQ,uint8_t velocidadMotorDER,uint8_t si,uint8_t sd,uint8_t cen,uint8_t INI2,uint8_t INI3,uint8_t INI4,uint8_t INI5, int velocidadIzq,int velocidadDer);
+    robotLinea(uint8_t velocidadMotorIZQ,uint8_t velocidadMotorDER,uint8_t si,uint8_t sd,uint8_t cen,uint8_t INI2,uint8_t INI3,uint8_t INI4,uint8_t INI5, uint8_t velocidadIzq,uint8_t velocidadDer);
     void estado();
     void correr();
     void giro_izquierda();
